db3523468ef1866e6c0493ad9b19c4cba8d606e5
contihan <daniel.hanak@outlook.cz>
2022-05-21 11:19:44 +0200

    Přidejte soubory projektu.

---
+++ Heartbeat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartbeatService
+{
+    public class Heartbeat
+    {
+        private readonly System.Timers.Timer timer;
+
+        public Heartbeat()
+        {
+            timer = new(1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Temp\ServiceDemo\Heartbeat.txt", lines);
+        }
+
+        public void Start() { timer.Start(); }
+        public void Stop() { timer.Stop(); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using HeartbeatService;
+using Topshelf;
+
+var exitCode = HostFactory.Run(x =>
+{
+    x.Service<Heartbeat>(s =>
+    {
+        s.ConstructUsing(heartbeat => new Heartbeat());
+        s.WhenStarted(heartbeat => heartbeat.Start());
+        s.WhenStopped(heartbeat => heartbeat.Stop());
+    });
+
+    x.RunAsLocalSystem();
+
+    x.SetServiceName("HeartbeatService");
+    x.SetDisplayName("Heartbeat Service");
+    x.SetDescription("This is my first windows service using Youtube tutorial");
+});
+
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.ExitCode = exitCodeValue;
